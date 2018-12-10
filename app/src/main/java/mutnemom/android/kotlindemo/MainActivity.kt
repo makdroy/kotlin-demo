ed6b812584b6c24920158bdb54786925955d37a3
Mutnemom <tt_yordkam@hotmail.com>
2018-12-11 00:37:59 +0700

    Initial commit.

---
+++ app/src/main/java/mutnemom/android/kotlindemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package mutnemom.android.kotlindemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
