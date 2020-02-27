1fe7d937e980e2827da5c5ea0f9f6c4ce210a2bc
Mutnemom <tt_yordkam@hotmail.com>
2020-02-27 11:21:00 +0700

    Record tts content to file then play audio playback with MediaPlayer

--- app/src/main/java/mutnemom/android/kotlindemo/tts/TextToSpeechActivity.kt
+++ app/src/main/java/mutnemom/android/kotlindemo/tts/TextToSpeechActivity.kt
@@ -1,11 +1,16 @@
 package mutnemom.android.kotlindemo.tts
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import kotlinx.android.synthetic.main.activity_text_to_speech.*
 import mutnemom.android.kotlindemo.R
+import java.io.File
 import java.util.*
 
 class TextToSpeechActivity : AppCompatActivity() {
@@ -32,29 +37,69 @@ class TextToSpeechActivity : AppCompatActivity() {
         }
     }
 
+    private val utteranceProgressListener = object : UtteranceProgressListener() {
+        override fun onStart(utteranceId: String?) {
+            Log.e("utterance", "-> onStart()")
+        }
+
+        override fun onDone(utteranceId: String?) {
+            Log.e("utterance", "-> onDone()")
+
+            if (utteranceId == synthesizeId) {
+                Log.e("utterance", "-> play audio")
+                playAudio()
+            }
+        }
+
+        override fun onError(utteranceId: String?) {
+            Log.e("utterance", "-> onError()")
+        }
+    }
+
+    private val synthesizeId = UUID.randomUUID().toString()
+    private lateinit var wavFile: File
+
+    private var player: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_to_speech)
 
+        wavFile = File("${applicationContext.getExternalFilesDir(null)}/tts_backup.wav")
+
         tts = TextToSpeech(this, ttsListener)
+        tts?.setOnUtteranceProgressListener(utteranceProgressListener)
 
         btnStart?.setOnClickListener { speakOut() }
-        btnStop?.setOnClickListener { tts?.stop() }
+        btnStop?.setOnClickListener { player?.pause() }
     }
 
     override fun onDestroy() {
+        player?.apply {
+            stop()
+            reset()
+        }
+
         tts?.apply {
             stop()
             shutdown()
         }
+
         super.onDestroy()
     }
 
     private fun speakOut() {
-        txtSample?.text?.toString()?.let {
-            tts?.speak(it, TextToSpeech.QUEUE_FLUSH, null, UUID.randomUUID().toString())
+        when (player) {
+            null -> txtSample?.text?.toString()?.let {
+                tts?.synthesizeToFile(it, null, wavFile, synthesizeId)
+            }
+            else -> player!!.start()
         }
+    }
 
+    private fun playAudio() {
+        player = MediaPlayer.create(this, wavFile.toUri())
+        player?.start()
     }
 
 }
