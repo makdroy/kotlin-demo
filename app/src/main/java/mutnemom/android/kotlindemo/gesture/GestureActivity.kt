c09ba9f51cb3ac340edaa4af8f23c761fff2ad96
Mutnemom <tt_yordkam@hotmail.com>
2020-04-29 18:10:30 +0700

    Implement simple touch listener

--- app/src/main/java/mutnemom/android/kotlindemo/gesture/GestureActivity.kt
+++ app/src/main/java/mutnemom/android/kotlindemo/gesture/GestureActivity.kt
@@ -1,7 +1,9 @@
 package mutnemom.android.kotlindemo.gesture
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_gesture.*
 import mutnemom.android.kotlindemo.R
 
 class GestureActivity : AppCompatActivity() {
@@ -9,5 +11,15 @@ class GestureActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gesture)
+
+        setEvent()
     }
+
+    private fun setEvent() {
+        touchArea?.setOnTouchListener { v, event ->
+            Log.e("tt", "-> handle touch event")
+            true
+        }
+    }
+
 }
