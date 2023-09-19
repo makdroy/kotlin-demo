132912a33e8b727658139f60b9781cc3ff0ee1e6
Mutnemom <tt_yordkam@hotmail.com>
2023-09-19 18:02:44 +0700

    Create ExampleUnitTest.kt

---
+++ app/src/test/java/mutnemom/android/kotlindemo/ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+package mutnemom.android.kotlindemo
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+}
